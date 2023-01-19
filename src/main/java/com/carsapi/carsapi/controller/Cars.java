11033856ca9604b1bd9e134fa0f317c6bd4e096a
Luís Henrique <luishsilva09@gmail.com>
2023-01-19 14:56:39 -0300

    deletar dado

--- src/main/java/com/carsapi/carsapi/controller/Cars.java
+++ src/main/java/com/carsapi/carsapi/controller/Cars.java
@@ -3,7 +3,9 @@ package com.carsapi.carsapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,10 @@ public class Cars {
     @GetMapping
     public List<Car> listAll(){
         return repository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        repository.deleteById(id);
     }
 }
